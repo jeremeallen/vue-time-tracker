40f8d509c3c96ade5c22da660bb338bd57b719a1
Jereme Allen <jereme.allen@wheniwork.com>
2017-04-12 17:51:55 -0500

    First pass through tutorial

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Hello from '@/components/Home';
+import LogTime from '@/components/LogTime';
+import TimeEntries from '@/components/TimeEntries';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Hello',
+      path: '/hello',
+      name: 'Home',
       component: Hello,
+    },
+    {
+      path: '/time-entries',
+      name: 'TimeEntries',
+      component: TimeEntries,
+      subRoutes: {
+        '/log-time': {
+          component: LogTime,
+          name: 'logtime',
+        },
+      },
+    },
+    {
+      path: '*',
+      redirect: '/hello',
     },
   ],
 });
